291b4f42c4272b18b27c976d44b1f92323935d53
GuiM0x <martox.lintox@gmail.com>
2018-01-30 17:40:20 +0100

    Small update

--- BlockBreaker/main.cpp
+++ BlockBreaker/main.cpp
@@ -6,15 +6,43 @@
 #include "include/Outils.h"
 
 ///////////////////////////////////////////////////
+/////// CONST
+const unsigned WINDOW_W = 1024;
+const unsigned WINDOW_H = 576;
+const float       PAD_W = 150.f;
+const float       PAD_H = 25.f;
+const float       PAD_Y = WINDOW_H - (PAD_H + 10.f);
+
+///////////////////////////////////////////////////
 /////// ENUM KEYS
 enum keys{LEFT, RIGHT, KEY_MAX};
 std::vector<bool> key(KEY_MAX, false);
 
 ///////////////////////////////////////////////////
+/////// CLASS PADDLE (PROTO)
+class Paddle : public sf::RectangleShape
+{
+public:
+    explicit Paddle(const sf::Vector2f& size);
+
+private:
+
+};
+/////// CLASS PADDLE (DEF)
+Paddle::Paddle(const sf::Vector2f& size) :
+    sf::RectangleShape{size}
+{
+
+}
+///////////////////////////////////////////////////
 /////// MAIN
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1024, 576), "Block Breaker");
+    sf::RenderWindow window(sf::VideoMode(1024, 576), "Block Breaker", sf::Style::Close);
+
+    /////// Paddle
+    Paddle pad{sf::Vector2f(PAD_W, PAD_H)};
+    pad.setPosition(0.f, PAD_Y);
 
     /////// CLOCK/DT
     sf::Clock clock;
@@ -33,10 +61,10 @@ int main()
             if(event.type == sf::Event::KeyPressed)
             {
                 //Directions
-				if (event.key.code == sf::Keyboard::Q) { key[LEFT]  = true; }
-				if (event.key.code == sf::Keyboard::D) { key[RIGHT] = true; }
+                if (event.key.code == sf::Keyboard::Q) { key[LEFT]  = true; }
+                if (event.key.code == sf::Keyboard::D) { key[RIGHT] = true; }
 
-				if(event.key.code == sf::Keyboard::Escape)
+                if(event.key.code == sf::Keyboard::Escape)
                     window.close();
             }
 
@@ -44,8 +72,8 @@ int main()
 			if (event.type == sf::Event::KeyReleased)
 			{
 				//Directions
-				if (event.key.code == sf::Keyboard::Q) { key[LEFT]  = false; }
-				if (event.key.code == sf::Keyboard::D) { key[RIGHT] = false; }
+                if (event.key.code == sf::Keyboard::Q) { key[LEFT]  = false; }
+                if (event.key.code == sf::Keyboard::D) { key[RIGHT] = false; }
 
 			}
 
@@ -68,7 +96,7 @@ int main()
 
         /////// DRAW
         window.clear();
-
+        window.draw(pad);
         window.display();
     }
 
